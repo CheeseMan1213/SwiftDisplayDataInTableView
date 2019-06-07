fb8baf9e7cf7f356dcd4bb98a6a79f6c1b2249ee
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-06-06 22:32:19 -0500

    Added the table view flowed by fixing the table view on a different day. Added some missing inheritance. Initialized dataSource and delegate with 'self'. I capitalized the character 'o' in the word 'of' for the identifier 'numberOfSections'. Added the letter 's' to the end of the word 'Row' identifier 'numberOfRowsInSection'

--- DisplayDataInTableView/ViewController.swift
+++ DisplayDataInTableView/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let ponieArray = ["Twilight Sparkle", "Fluttershy",
+                       "Apple Jack", "Pinkie Pie", "Rarity", "Ranbow Dash"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
-
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ponieArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: performSegue(withIdentifier: "MyCell", for: indexPath))
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        cell.textLabel!.text = ponieArray[indexPath.row]
+        
+        return cell
+        
+    }
 }
-
